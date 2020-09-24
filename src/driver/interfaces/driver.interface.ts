0b2acde888a1f4d611b7683f22a0bbd9dd3b4111
salbador <salbador@centroderentas.mx>
2020-09-24 16:22:03 +0200

    generated successfully basic skelen from the list batch template to each

--- src/driver/interfaces/driver.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface Driver extends Document {
-  readonly name: string;
-  readonly description: string;
-  readonly imageURL: string;
-  readonly price: number;
-  readonly createdAt: Date;
-}
