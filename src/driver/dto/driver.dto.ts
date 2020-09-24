5720808b7095854c1e4523067aa67cd436cebf10
salbador <salbador@centroderentas.mx>
2020-09-24 16:22:03 +0200

    generated successfully basic skelen from the list batch template to each

--- src/driver/dto/driver.dto.ts
+++
@@ -1,8 +0,0 @@
-export default class CreateDriverDTO {
-  readonly name: string;
-  readonly description: string;
-  readonly imageURL: string;
-  readonly price: number;
-  readonly createdAt: Date;
-
-}
