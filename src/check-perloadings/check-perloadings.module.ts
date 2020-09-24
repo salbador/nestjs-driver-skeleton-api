e6e152f94458f5297667991e28e760235f41f8d0
salbador <salbador@centroderentas.mx>
2020-09-24 16:22:02 +0200

    generated successfully basic skelen from the list batch template to each

--- src/check-perloadings/check-perloadings.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CheckPerloadingsController } from './check-perloadings.controller';
-import { CheckPerloadingsService } from './check-perloadings.service';
-
-@Module({
-  controllers: [CheckPerloadingsController],
-  providers: [CheckPerloadingsService]
-})
-export class CheckPerloadingsModule {}
