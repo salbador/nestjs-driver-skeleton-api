3b680f663fbab3503fc2f72beec273cd645dde98
salbador <salbador@centroderentas.mx>
2020-09-24 16:22:04 +0200

    generated successfully basic skelen from the list batch template to each

--- src/get-user-data-by-driver-pin/get-user-data-by-driver-pin.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GetUserDataByDriverPinController } from './get-user-data-by-driver-pin.controller';
-import { GetUserDataByDriverPinService } from './get-user-data-by-driver-pin.service';
-
-@Module({
-  controllers: [GetUserDataByDriverPinController],
-  providers: [GetUserDataByDriverPinService]
-})
-export class GetUserDataByDriverPinModule {}
